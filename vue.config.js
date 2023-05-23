2e97244b73a24ad2f9c90fedc63973899d7df057
lytvyniuk_v <lv@pinpools.com>
2023-05-23 16:50:52 +0300

    Created task.

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+    loaderExports: {
+      sass: {
+         prependData: '@import "@/assets/styles.scss";'
+      }
+    }
+  }
+}
